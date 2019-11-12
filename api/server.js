75d86c85fd442d2fd2e0859334631fa6c589958a
CScori <49799016+CScori@users.noreply.github.com>
2019-11-12 15:15:43 -0600

    adds in new modules for use

--- api/server.js
+++ api/server.js
@@ -1,14 +1,25 @@
 const express = require('express')
+const helmet  = require('helmet')
+const cors  = require('cors')
+const session = require('express-session')
+const KSessStore = require('connect-session storage')
 
 
 const enter = require('./a-router.js')
-const mids = require('/fig-mids.js');
+const logs = require('../loggers/logger-router');
 
 const server = express()
 
 mids(server)
 
 server.use('/api', enter)
+server.use(express.json())
+server.use(cors())
 
+server.use('/api/auth', enter)
+server.use('/api/user', logs)
 
+server.get('/', (req, res) => {
+    res.send('Welcome, ask for something')
+})
 module.exports = server
