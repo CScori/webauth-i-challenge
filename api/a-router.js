d7d3e2b05b6608061bd0c51e7db0501e7c0e65f6
CScori <49799016+CScori@users.noreply.github.com>
2019-11-12 16:29:31 -0600

    fixes routed from yesterday

--- api/a-router.js
+++ api/a-router.js
@@ -1,25 +1,66 @@
-const bcrypt = require('bcryptjs'); // npm i bcryptjs
-
 const router = require('express').Router();
-
+const bcrypt = require('bcryptjs'); // npm i bcryptjs
 
 // reg and login rt
 const author = require('../auths/auth-rtr.js');
 const logger = require('../loggers/logger-router.js')
 
-router.use('/auth', author)
-router.use('/login', logger)
+const db = require('../loggers/login-model.JS')
 
-router.get('/', (req, res) => {
-    res.json({ api: "Scan your 6@rc0d3"})
-})
-
-router.post('/hash', (req, res) => {
-    const pass = req.body.password
-
-    const hash = bcrypt.hashSync(pass, 12)
-
-    res.status(200).json({ pass, hash})
-})
-
-module.exports = router
+router.post("/register", (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+    user.password = hash;
+  
+    db.add(user)
+      .then(saved => {
+        req.session.username = saved.username;
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  router.post("/login", (req, res) => {
+    let { username, password } = req.body;
+  
+    db.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          req.session.username = user.username; // << good: add properties to existing session object
+          // req.session = { username: user.username } // bad panda: don't override the session object
+          res.status(200).json({
+            message: `Welcome ${user.username}!`
+          });
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  router.get("/logout", (req, res) => {
+    if (req.session) {
+      req.session.destroy(error => {
+        if (error) {
+          res
+            .status(500)
+            .json({
+              message:
+                "you can check out any time you like, but you can never leave..."
+            });
+        } else {
+          res.status(200).json({ message: "logged out successfully" });
+        }
+      });
+    } else {
+      res.status(200).json({ message: "by felicia" });
+    }
+  });
+  
+  module.exports = router;
+  
