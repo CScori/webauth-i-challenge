0029ecdd11dfda83913367f527fcfadd5120b077
CScori <49799016+CScori@users.noreply.github.com>
2019-11-12 15:25:34 -0600

    adds sesh figure

--- api/server.js
+++ api/server.js
@@ -3,19 +3,33 @@ const helmet  = require('helmet')
 const cors  = require('cors')
 const session = require('express-session')
 const KSessStore = require('connect-session storage')
-
-
 const enter = require('./a-router.js')
 const logs = require('../loggers/logger-router');
-
 const server = express()
 
-mids(server)
+const sKfig = {
+    name: 'logger',
+    secret:  process.env.COOKIE_S || "private note",
+    cookie: {
+       maxAge: 1000 * 60 * 60, 
+       secure: process.env.NODE_ENV ==='dev'? false : true, 
+       httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new KSessStore({
+        knex: knexConnection,
+        clearInterval: 1000 * 60 * 10,
+        tablename: 'log_sesh',
+        sidfieldname: "id",
+        createtable: true
+    })
+}
 
-server.use('/api', enter)
 server.use(express.json())
 server.use(cors())
-
+server.use(helmet())
+server.session(session())
 server.use('/api/auth', enter)
 server.use('/api/user', logs)
 
